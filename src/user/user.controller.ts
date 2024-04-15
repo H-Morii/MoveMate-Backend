11051760e9120030bd43aa60d37b91d6a606abed
H-morii <haruki.mori21@gmail.com>
2024-04-15 16:16:25 +0900

    User controller completed

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -46,9 +46,22 @@ export const deleteUser =async (req:Request, res:Response) => {
   }
 }
 
-
-
-
-
+export const updateUser =async (req:Request, res:Response) => {
+  try {
+    
+    const data = req.body
+    const id = parseInt(req.body.id)
+    const user = await UserModel.patchUser(id, data);
+    if(!user) {
+      res.status(400).json({message: "User not found"})
+    } else {
+      res. status(200).json({message: "User us now updated"})
+    }
+  } catch (err:any) {
+    console.error(err.message);
+    res.status(500).json({message: err.message})
+    
+  }
+}
 
 
